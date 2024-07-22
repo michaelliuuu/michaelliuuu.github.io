cdffe977a3b50ab1e18562b0ee4745887709c58a
Michael Liu <102439915+michaelliuuu@users.noreply.github.com>
2024-07-22 13:04:51 -0700

    Layout for contact page

--- scripts.js
+++ scripts.js
@@ -4,3 +4,44 @@ fetch('header.html')
     .then(data => {
         document.getElementById('header').innerHTML = data;
     });
+
+//PhoneNum and Email copies into clipboard when clicked
+const copyPhoneNum = document.getElementById('phoneNum');
+const copyMyEmail = document.getElementById('myemail');
+const text1 = '+1(778)-927-5568';
+const text2 = 'michaelliu.98@hotmail.com';
+
+function copyText(indicator) {
+    if (indicator == 0) {
+        popup(text1);
+    } else {
+        popup(text2);
+    }
+}
+
+function popup(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        const popup = document.getElementById("popup");
+        // Show the popup
+        popup.style.visibility = 'visible';
+        popup.style.opacity = 1;
+
+        // Hide the popup after 2 seconds
+        setTimeout(() => {
+            popup.style.opacity = 0;
+            popup.style.visibility = 'hidden';
+        }, 2000);
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+}
+
+copyPhoneNum.addEventListener('click', function(event) {
+    event.preventDefault();
+    copyText(0);
+});
+
+copyMyEmail.addEventListener('click', function(event) {
+    event.preventDefault();
+    copyText(1);
+});
